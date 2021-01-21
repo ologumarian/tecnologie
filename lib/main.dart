87519fd74f87bc47e52bcc3776d6007406e90ffb
Marian Ologu <ologumarian@icloud.com>
2021-01-21 23:09:31 +0100

    Gio 21 gen 2021

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Documentive',
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Documentive'),
     );
   }
 }
