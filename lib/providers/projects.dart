46e2bcb9e1386fa69941886f559419e9aa643529
Marian Ologu <ologumarian@icloud.com>
2021-02-28 11:18:58 +0100

    project saved to local list

--- lib/providers/projects.dart
+++ lib/providers/projects.dart
@@ -27,44 +27,48 @@ class Projects with ChangeNotifier {
     return _username;
   }
 
-  Future<void> fetchAndSetProjects() {}
+  Future<void> fetchAndSetProjects() {
+    return null;
+  }
 
   Future<void> addProject({String name, String desc, String imageLink}) {
     var date = DateTime.now();
-    Project item = Project();
+    var item = Project();
 
     CollectionReference projects = FirebaseFirestore.instance
         .collection('users')
         .doc(_uid)
         .collection('projects');
 
-    projects.add({
+    final response = projects.add({
       'name': name,
       'date': date,
       'description': desc,
       'imageLink': imageLink,
       'owner': _username,
-    });
-    // .then((value) {
-    //   item = Project(
-    //     id: value.id,
-    //     name: name,
-    //     date: date,
-    //     description: desc,
-    //     imageLink: imageLink,
-    //     owner: username,
-    //   );
-    //   _items.add(item);
-    // }).catchError(() => print('ERRORE ADD_PROJECT SU FIRESTORE'));
+    }).then((value) {
+      print('FIRESTORE DOCUMENT ID: ' + value.id);
+      item = Project(
+        id: value.id,
+        name: name,
+        date: date,
+        description: desc,
+        imageLink: imageLink,
+        owner: username,
+      );
+      _items.insert(0, item);
+
+      print('NOME: ' + item.name);
+      print('DATA: ' + item.date.toIso8601String());
+      print('DESCRIZIONE: ' + item.description);
+      print('LINK: ' + item.imageLink);
+      print('PROPRIETARIO: ' + item.owner);
+      print('--> NUMERO PROGETTI: ' + items.length.toString());
+    }).catchError(
+      (onError) => print('ERRORE ADD_PROJECT SU FIRESTORE ' + onError),
+    );
 
     print('PROGETTO AGGIUNTO A FIRESTORE');
-
-    // print(item.name);
-    // print(item.date);
-    // print(item.description);
-    // print(item.imageLink);
-    // print(item.owner);
-    // print('Numero progetti:' + items.length.toString());
     return null;
   }
 }
